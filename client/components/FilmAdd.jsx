a20fbe6aa39a113606ec6dd8369e5335492e2f57
GraceAgony <theogonia8@gmail.com>
2017-08-22 06:19:27 -0700

    add validation

--- client/components/FilmAdd.jsx
+++ client/components/FilmAdd.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './FilmAdd.css';
+import Formsy from 'formsy-react';
 
 import Modal from 'react-modal';
 
@@ -9,10 +10,13 @@ class FilmAdd extends React.Component{
         this.state={
             title: '',
             format: '',
-            releaseYear: 0,
+            releaseYear: '',
             stars: '',
-             isOpen: false
+            isOpen: false,
+            canSubmit: false,
         };
+
+
 
         this.handleChangeFormat = this.handleChangeFormat.bind(this);
         this.handleChangeTitle = this.handleChangeTitle.bind(this);
@@ -23,7 +27,8 @@ class FilmAdd extends React.Component{
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
 
-
+        this.enableButton = this.enableButton.bind(this);
+        this.disableButton = this.disableButton.bind(this);
     };
 
     handleChangeTitle(event){
@@ -62,10 +67,16 @@ class FilmAdd extends React.Component{
             stars: this.state.stars,
             sortFileld: this.state.title.toLowerCase(),
         };
-
         this.props.onFilmAdd(newFilm);
 
         this.handleCloseModal();
+    };
+
+    enableButton() {
+        this.setState({ canSubmit: true });
+    };
+    disableButton() {
+        this.setState({ canSubmit: false });
     };
 
     render(){
@@ -77,21 +88,43 @@ class FilmAdd extends React.Component{
                 contentLabel="Modal"
                 className='Modal'
             >
-                <form id="addFilmForm">
-                    <img id="closeImg" onClick={this.handleCloseModal} src={require('../image/cross.png')}/>
-                    <div id="inputForm">
-                        <input className="labelInput" type="text" placeholder="Enter title" value={this.state.title} onChange={this.handleChangeTitle} />
-                        <input id="numberInput" className="labelInput" type="number" placeholder="Enter release year" value={this.state.releaseYear} onChange={this.handleChangeRelease} />
-                        <input className="labelInput" type="text" placeholder="Enter format" value={this.state.format} onChange={this.handleChangeFormat} />
-                        <input className="labelInput" type="text" placeholder="Enter stars" value={this.state.stars} onChange={this.handleChangeStars} />
-                    </div>
-                </form>
-                <input id="addFilmButton"  className="btn" type="submit" value="Submit" onClick={this.handleSubmit}/>
-
+                <div id="closeDiv">
+                <img id="closeImg" onClick={this.handleCloseModal} src={require('../image/cross.png')}/>
+                </div>
+                <Formsy.Form id="addFilmForm" onSubmit={this.handleSubmit} onValid={this.enableButton} onInvalid={this.disableButton} >
+                    <MyInput name="title" className="labelInput" type="text" placeholder="Enter title" value={this.state.title} onChange={this.handleChangeTitle} required />
+                    <MyInput name="year" id="numberInput" className="labelInput" validations="isNumeric" validationError="This is not a valid year" type="number" placeholder="Enter release year" value={this.state.releaseYear} onChange={this.handleChangeRelease} />
+                    <MyInput name="format" className="labelInput" type="text" placeholder="Enter format" value={this.state.format} onChange={this.handleChangeFormat} />
+                    <MyInput name="stars" className="labelInput" type="text" placeholder="Enter stars" value={this.state.stars} onChange={this.handleChangeStars} />
+                    <button id="addFilmButton"  className="btn" type="submit" disabled={!this.state.canSubmit} onClick={this.handleSubmit}>Submit</button>
+                </Formsy.Form>
             </Modal>
         </div>
         );
     }
 };
+
+const MyInput = React.createClass({
+    mixins: [Formsy.Mixin],
+    render() {
+        const className = 'form-group' + (this.props.className || ' ') + (this.showRequired() ? 'required' : this.showError() ? 'error' : null);
+        const errorMessage = this.getErrorMessage();
+        return (
+            <div className={className}>
+                <label htmlFor={this.props.name}>{this.props.title}</label>
+                <input
+                    type={this.props.type || 'text'}
+                    name={this.props.name}
+                    onChange={this.props.onChange}
+                    value={this.getValue()}
+                    checked={this.props.type === 'checkbox' && this.getValue() ? 'checked' : null}
+                />
+                <span ref="noValid" className='validation-error'>{errorMessage + (this.showRequired()&&(this.value === undefined)) ? "*it's required" : ''}</span>
+            </div>
+        );
+    }
+});
+
+
 
 export default FilmAdd;
